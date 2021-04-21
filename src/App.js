e299d848e5a861d0ea828a21b39632854ea9c352
arturovm2 <arturovm2@gmail.com>
2021-04-21 23:48:58 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
   return (
     <div className="App">
       <h1>Hello CodeSandbox test</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <span>{contador}</span>
+      <button onClick={() => setContador(contador + 1)}>contador</button>
     </div>
   );
 }
