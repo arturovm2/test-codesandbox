64a08ea71eb14340ef14c415d6fb4445251cc91f
arturovm2 <arturovm2@gmail.com>
2021-04-26 01:44:28 +0200

    especialisimo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
-import { useState } from "react";
 
+import { useState } from "react";
 export default function App() {
   const [contador, setContador] = useState(0);
+  const especialisimo = "especialisimo";
   return (
     <div className="App">
-      <h1>Hello CodeSandbox test</h1>
+      <h1>{`Hello CodeSandbox test ${especialisimo}`}</h1>
       <h2>Start editing to see some magic happen!</h2>
       <span>{contador}</span>
       <button onClick={() => setContador(contador + 1)}>contador</button>
